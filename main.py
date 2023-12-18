2c42531a25d35b9d814c93e7171c43c311aa4147
silentknight <1491628+silentknight@users.noreply.github.com>
2023-12-18 19:42:41 +0000

    Up

---
+++ main.py
@@ -0,0 +1,118 @@
+from datasets import load_metric
+from transformers import AutoTokenizer
+import un_ner_tokens as get_tokens
+from transformers import AutoModelForTokenClassification, TrainingArguments, Trainer
+from transformers import DataCollatorForTokenClassification
+import numpy as np
+
+import torch
+if torch.cuda.is_available():
+    print("torch CUDA is available")
+
+label_list = [
+    'O',       # Outside of a named entity
+    'B-MISC',  # Beginning of a miscellaneous entity right after another miscellaneous entity
+    'I-MISC',  # Miscellaneous entity
+    'B-PER',   # Beginning of a person's name right after another person's name
+    'I-PER',   # Person's name
+    'B-ORG',   # Beginning of an organisation right after another organisation
+    'I-ORG',   # Organisation
+    'B-LOC',   # Beginning of a location right after another location
+    'I-LOC'    # Location
+]
+label_encoding_dict = {'I-PRG': 2,'I-I-MISC': 2, 'I-OR': 6, 'O': 0, 'I-': 0, 'VMISC': 0, 'B-PER': 3, 'I-PER': 4, 'B-ORG': 5, 'I-ORG': 6, 'B-LOC': 7, 'I-LOC': 8, 'B-MISC': 1, 'I-MISC': 2}
+
+task = "ner"
+model_checkpoint = "xlnet-base-cased"
+batch_size = 16
+
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+def tokenize_and_align_labels(examples):
+    label_all_tokens = True
+    tokenized_inputs = tokenizer(list(examples["tokens"]), truncation=True, is_split_into_words=True)
+
+    labels = []
+    for i, label in enumerate(examples[f"{task}_tags"]):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:
+            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+            # ignored in the loss function.
+            if word_idx is None:
+                label_ids.append(-100)
+            elif label[word_idx] == '0':
+                label_ids.append(0)
+            # We set the label for the first token of each word.
+            elif word_idx != previous_word_idx:
+                label_ids.append(label_encoding_dict[label[word_idx]])
+            # For the other tokens in a word, we set the label to either the current label or -100, depending on
+            # the label_all_tokens flag.
+            else:
+                label_ids.append(label_encoding_dict[label[word_idx]] if label_all_tokens else -100)
+            previous_word_idx = word_idx
+
+        labels.append(label_ids)
+
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
+
+train_dataset, test_dataset = get_tokens.get_un_token_dataset('./UN-named-entity-recognition/tagged-training/', './UN-named-entity-recognition/tagged-test/')
+
+train_tokenized_datasets = train_dataset.map(tokenize_and_align_labels, batched=True)
+test_tokenized_datasets = test_dataset.map(tokenize_and_align_labels, batched=True)
+
+model = AutoModelForTokenClassification.from_pretrained(model_checkpoint, num_labels=len(label_list))
+
+args = TrainingArguments(
+    f"test-{task}",
+    evaluation_strategy = "epoch",
+    learning_rate=1e-4,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=10,
+    weight_decay=0.00001,
+)
+
+data_collator = DataCollatorForTokenClassification(tokenizer)
+metric = load_metric("seqeval")
+
+
+def compute_metrics(p):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=2)
+
+    # Remove ignored index (special tokens)
+    true_predictions = [
+        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    results = metric.compute(predictions=true_predictions, references=true_labels)
+    return {
+        "precision": results["overall_precision"],
+        "recall": results["overall_recall"],
+        "f1": results["overall_f1"],
+        "accuracy": results["overall_accuracy"],
+    }
+
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=train_tokenized_datasets,
+    eval_dataset=test_tokenized_datasets,
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics
+)
+
+trainer.train()
+
+trainer.evaluate()
+
+trainer.save_model('un-ner.model')
